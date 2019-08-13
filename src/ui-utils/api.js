fcb059625748961e874bfca9c82f05e538b50f66
muralimgwl <murali.m@goodworklabs.com>
2019-08-13 16:30:11 +0530

    added urlencoded content type

--- src/ui-utils/api.js
+++ src/ui-utils/api.js
@@ -4,14 +4,13 @@ import { prepareFinalObject } from "../ui-redux/screen-configuration/actions";
 import store from "../ui-redux/store";
 
 const getToken = () => {
-  const userInfo = JSON.parse(localStorage.getItem("user-info")) || {};
-  return userInfo.token;
+  const userInfo = JSON.parse(localStorage.getItem("userInfo")) || {};
+  return userInfo.session_id;
 };
 
 let axiosInstances = {
   instanceOne: axios.create({
-    baseURL: "",
-    // window.location.origin,
+    baseURL: window.location.origin,
     headers: {
       "Content-Type": "application/json"
     }
@@ -30,12 +29,13 @@ const wrapRequestBody = requestBody => {
 };
 
 export const httpRequest = async ({
-  method = "get",
+  method = "post",
   endPoint,
   queryObject = [],
   requestBody = {},
   instance = "instanceOne",
-  hasSpinner = true
+  hasSpinner = true,
+  contentType = "application/x-www-form-urlencoded"
 }) => {
   if (hasSpinner) {
     store.dispatch(prepareFinalObject("spinner", true));
@@ -43,8 +43,29 @@ export const httpRequest = async ({
   let apiError = "Api Error";
 
   var headerConfig = {
-    headers: { Authorization: `Bearer ${getToken()}` }
+    headers: {}
   };
+  if (contentType) {
+    switch (contentType) {
+      case "application/x-www-form-urlencoded":
+        var params = new URLSearchParams();
+        for (var variable in requestBody) {
+          if (requestBody.hasOwnProperty(variable)) {
+            params.append(variable, requestBody[variable]);
+          }
+        }
+        requestBody=params;
+        break;
+      default:
+
+    }
+    const session_id=getToken();
+    if (session_id) {
+      params.append("session.id", session_id);
+      params.append("session_id", session_id);
+    }
+    headerConfig.headers["Content-Type"] = contentType;
+  }
 
   endPoint = addQueryArg(endPoint, queryObject);
   var response;
@@ -71,9 +92,8 @@ export const httpRequest = async ({
     const { data, status } = error.response;
     if (status === 400 && data === "") {
       apiError = "INVALID_TOKEN";
-    }
-    else {
-      apiError=data;
+    } else {
+      apiError = data;
     }
     if (hasSpinner) {
       store.dispatch(prepareFinalObject("spinner", false));
