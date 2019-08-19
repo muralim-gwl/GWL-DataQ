75e3b16c73a9b369f04b12390b1405715ad9b283
muralimgwl <murali.m@goodworklabs.com>
2019-08-20 00:31:09 +0530

    added all components related phase 1

--- src/ui-config/flowComponents.js
+++ src/ui-config/flowComponents.js
@@ -6,29 +6,6 @@ const flowComponents=[
     type:"InputSource"
   },
   {
-    name:"Output Source",
-    icon:"/assets/images/svg_components/Output.svg",
-    hasInput:true
-  },
-  {
-    name:"Output Source",
-    icon:"/assets/images/svg_components/Output.svg",
-    hasInput:true,
-    maxConnections:4
-  },
-  {
-    name:"Column Type",
-    icon:"/assets/images/svg_components/Column Type.svg",
-    hasInput:true,
-    hasOutput:true
-  },
-  {
-    name:"Command",
-    icon:"/assets/images/svg_components/Command.svg",
-    hasInput:true,
-    hasOutput:true
-  },
-  {
     name:"Data Compare",
     icon:"/assets/images/svg_components/Data Compare.svg",
     hasInput:true,
@@ -40,15 +17,56 @@ const flowComponents=[
     hasInput:true,
     hasOutput:true
   },
+  // {
+  //   name:"Select Columns",
+  //   icon:"/assets/images/svg_components/Select Columns.svg",
+  //   hasInput:true,
+  //   hasOutput:true
+  // },
+  {
+    name:"Filter Rows",
+    icon:"/assets/images/svg_components/Filter Rows.svg",
+    hasInput:true,
+    hasOutput:true
+  },
+  {
+    name:"Column Type",
+    icon:"/assets/images/svg_components/Column Type.svg",
+    hasInput:true,
+    hasOutput:true
+  },
   {
     name:"Join Data",
     icon:"/assets/images/svg_components/Join Data.svg",
     hasInput:true,
     hasOutput:true
   },
+  // {
+  //   name:"Remove Duplicates",
+  //   icon:"/assets/images/svg_components/Remove Duplicates.svg",
+  //   hasInput:true,
+  //   hasOutput:true
+  // },
+  // {
+  //   name:"Text Formatting",
+  //   icon:"/assets/images/svg_components/Text Formatting.svg",
+  //   hasInput:true,
+  //   hasOutput:true
+  // },
+  // {
+  //   name:"Replace Text",
+  //   icon:"/assets/images/svg_components/Replace Text.svg",
+  //   hasInput:true,
+  //   hasOutput:true
+  // },
   {
-    name:"Select Columns",
-    icon:"/assets/images/svg_components/Select Columns.svg",
+    name:"Output Source",
+    icon:"/assets/images/svg_components/Output.svg",
+    hasInput:true
+  },
+  {
+    name:"Command",
+    icon:"/assets/images/svg_components/Command.svg",
     hasInput:true,
     hasOutput:true
   }
